e749634480dcb46cc7a96d531e43b9a2fb380484
unknown <miratkhalikov007@gmail.com>
2026-09-25 16:09:41 +0500

    [TASK-1] bad implementation of calc

--- mirat.khalikov/task-1/cmd/service/main.go
+++ mirat.khalikov/task-1/cmd/service/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b, res int
+	var op string
+
+	fmt.Scanln(&a)
+	fmt.Scanln(&op)
+	fmt.Scanln(&b)
+
+	switch op {
+	case "+":
+		res = a + b
+	case "-":
+		res = a - b
+	case "*":
+		res = a * b
+	case "/":
+		res = a / b
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+	fmt.Println(res)
 }
